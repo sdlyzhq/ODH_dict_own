46af2ee6d86af088b8b6b35e97cb9ad7515b2191
Hanqing Zhao <45854215+sdlyzhq@users.noreply.github.com>
2022-06-13 17:33:44 +0800

    Update encn_LDOCE6MDX.js

--- encn_LDOCE6MDX.js
+++ encn_LDOCE6MDX.js
@@ -62,7 +62,7 @@ class encn_LDOCE6MDX {
             return value;
         }
 
-        let base = 'https://127.0.0.1:8000/';
+        let base = 'https://localhost:8000/';
         let url = base + encodeURIComponent(word);
         let doc = '';
         try {
